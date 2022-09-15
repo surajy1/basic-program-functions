3203315efd8770cdb21ad237f749eb17cd1560f7
surajy1 <76870806+surajy1@users.noreply.github.com>
2022-09-15 19:29:37 +0530

    Create power.c
    Program to calculate the power of a number.

---
+++ power.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int power(int base, int exponent){
+    
+    int base1 = 1;
+    
+    for(int i=1; i<=exponent; i++)
+    {
+        base1 = base1 * base;
+    }
+    
+    printf("Answer: %d", base1);
+    
+}
+
+int main() {
+    
+    int base, exponent;
+    
+    printf("Enter the base: ");
+    scanf("%d", &base);
+    
+    printf("Enter the exponent: ");
+    scanf("%d", &exponent);
+    
+    power(base, exponent);
+
+    return 0;
+}
